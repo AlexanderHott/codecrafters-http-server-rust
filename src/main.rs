5c23bdeded1a218f6b2383a0548db426de732f02
AlexanderHOtt <66271487+AlexanderHOtt@users.noreply.github.com>
2023-10-21 19:38:04 -0400

    start a tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 // Uncomment this block to pass the first stage
 // use std::net::TcpListener;
 
+use std::net::TcpListener;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("0.0.0.0:4221").unwrap();
+    listener.incoming().for_each(|stream| {
+        match stream {
+            Ok(_) => eprintln!("new conn"),
+            Err(e) => eprintln!("error {e}"),
+        }
+    })
 
     // Uncomment this block to pass the first stage
     //
