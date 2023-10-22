073afb72cc52548d3f9dd571ec48f0c0cfcd7c96
AlexanderHOtt <66271487+AlexanderHOtt@users.noreply.github.com>
2023-10-21 21:17:44 -0400

    parse headers

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use anyhow::anyhow;
 use std::{
     io::{Read, Write},
     net::TcpListener,
-    str::FromStr,
+    str::FromStr, collections::HashMap,
 };
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
@@ -64,6 +64,26 @@ impl FromStr for StatusLine {
     }
 }
 
+#[derive(Debug)]
+struct Headers(HashMap<String, String>);
+
+impl FromStr for Headers {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut hm = HashMap::new();
+        eprintln!("Parsing headers {s}");
+
+        for line in s.split("\r\n").collect::<Vec<_>>() {
+            if let Some((key, val)) = line.split_once(": ") {
+                hm.entry(key.to_string()).and_modify(|s| *s = val.to_string() ).or_insert(val.to_string());
+            }
+        }
+
+        Ok(Self(hm))
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -73,36 +93,45 @@ fn main() -> anyhow::Result<()> {
             Ok(mut stream) => {
                 let mut buf = [0u8; 1024];
                 let read = stream.read(&mut buf).unwrap();
-                if let Some((status_line, rest)) = String::from_utf8(buf[..read].to_vec())
-                    .unwrap()
-                    .split_once("\r\n")
-                {
-                    let status_line = StatusLine::from_str(status_line)?;
-                    eprintln!("Read {read} bytes {status_line:?}");
 
-                    let mut response = String::new();
-                    eprintln!("{:?}", status_line);
-                    let path_parts = status_line.path.split("/").collect::<Vec<_>>();
-                    eprintln!("{:?}", path_parts);
-                    match path_parts[1] {
-                        "" => {
-                            response.push_str("HTTP/1.1 200 OK\r\n\r\n");
-                        }
-                        "echo" => {
-                            let s = &path_parts[2..].join("/");
-                            eprintln!("s {s}");
-                            response.push_str(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", s.len(), s).as_str());
-                        }
-                        p => {
-                            eprintln!("Not route for {}", p);
-                            response.push_str("HTTP/1.1 404 Not Found\r\n\r\n");
-                        }
-                    };
-                    eprintln!("writing response\n{response}");
-                    let written = stream.write(response.as_bytes()).unwrap();
-                    stream.flush().unwrap();
-                    eprintln!("Wrote {written} bytes");
-                }
+                let req_str = String::from_utf8(buf[..read].to_vec())?;
+                let (status_line, rest) = req_str.split_once("\r\n").unwrap();
+
+                let req = rest.split("\r\n\r\n").collect::<Vec<_>>();
+                let headers = req[0];
+                // let body = req[2];
+                eprintln!("Read {read} bytes {status_line:?} {headers:?}");
+
+                let status_line = StatusLine::from_str(status_line)?;
+                let headers = Headers::from_str(headers)?;
+                eprintln!("Read {read} bytes {status_line:?} {headers:?}");
+
+                let mut response = String::new();
+                eprintln!("{:?}", status_line);
+                let path_parts = status_line.path.split("/").collect::<Vec<_>>();
+                eprintln!("{:?}", path_parts);
+                match path_parts[1] {
+                    "" => {
+                        response.push_str("HTTP/1.1 200 OK\r\n\r\n");
+                    }
+                    "echo" => {
+                        let s = &path_parts[2..].join("/");
+                        eprintln!("s {s}");
+                        response.push_str(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", s.len(), s).as_str());
+                    }
+                    "user-agent" => {
+                        let s = headers.0.get("User-Agent").unwrap();
+                        response.push_str(format!("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", s.len(), s).as_str());
+                    }
+                    p => {
+                        eprintln!("Not route for {}", p);
+                        response.push_str("HTTP/1.1 404 Not Found\r\n\r\n");
+                    }
+                };
+                eprintln!("writing response\n{response}");
+                let written = stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
+                eprintln!("Wrote {written} bytes");
             }
             Err(e) => eprintln!("error {e}"),
         }
